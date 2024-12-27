b30e2f38ffd805336aaea9b889a9659b55af62e0
Junsoo Kim <kel_777@naver.com>
2024-12-27 14:05:56 +0900

    수정 버튼 만드는중 일단 푸쉬하기

--- js/kimjunsoo.js
+++ js/kimjunsoo.js
@@ -29,18 +29,41 @@ function addTodo(text, checked = false) {
         saveTodos(todos);
     });
 
-    // 삭제 버튼 만들기
+    // ++수정 버튼 만들기 - 아직 미완성
+    const editBtn = document.createElement('button');
+    editBtn.textContent = '수정';
+    editBtn.addEventListener('click', ()=>{
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = text; // 저장 되어있는 텍스트 호출
+        li.replaceChild(input, li.childNodes[1]); // 텍스트 수정
+
+        const saveBtn = document.createElement('button');
+        saveBtn.textContent = '저장';
+        saveBtn.addEventListener('click', ()=>{
+            if (todoInput.value.trim() === '') {
+                alert('할 일을 입력 해주세요!');
+                return; // 빈 입력 방지
+            };
+            
+        });
+
+    });
+
+    // ++삭제 버튼 만들기
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = '삭제';
     deleteBtn.addEventListener('click', ()=> {
         const todos = loadTodos();
         const index = Array.from(li.parentElement.children).indexOf(li);
-        todos.splice(index, 1);
-        saveTodos(todos);
-        li.remove();
+        todos.splice(index, 1); // 할 일 제거
+        saveTodos(todos);   // 저장
+        li.remove();   // 보여지는 화면에서 삭제    
     });
 
+    // ul > li에 추가한 요소
     li.prepend(checkbox);
+    li.append(editBtn);
     li.append(deleteBtn);
     todoListElement.append(li);
 }
@@ -66,8 +89,11 @@ function initialize() {
 
     // 새로운 할일 추가 버튼 클릭 이벤트
     addButton.addEventListener('click', () => {
-        if (todoInput.value.trim() === '') return; // 빈 입력 방지
-        
+        if (todoInput.value.trim() === '') {
+            alert('할 일을 입력 해주세요!');
+            return; // 빈 입력 방지
+        };
+
         // 새로운 할일 추가
         addTodo(todoInput.value);
 
